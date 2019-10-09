bbf478a039fea38a059d92761edbcf4b3dd4e5eb
HamidReza Nasrollahi <nasrollahy.hamidreza@gmail.com>
2019-10-09 14:02:51 +0330

    test ai

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const brain = require("brain.js");
 const network = new brain.NeuralNetwork();
 
 network.train([
-    { input: [0, 0, 0], output: [0] },
-    { input: [0, 0, 1], output: [0] },
-    { input: [0, 1, 1], output: [0] },
-    { input: [1, 0, 1], output: [1] },
-    { input: [1, 1, 1], output: [1] }
+    { input: ["a", "b", "t"], output: [0] },
+    { input: ["b", "a", "t"], output: [0] },
+    { input: ["t", "b", "a"], output: [0] },
+    { input: ["t", "a", "b"], output: [1] },
+    { input: ["b", "t", "a"], output: [1] }
 ]);
 
-const output = network.run([1, 0, 0]);
+const output = network.run(["a", "t", "b"]);
 
 console.log(`resolve: ${output}`);
